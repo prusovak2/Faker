54f70be1343cc5df3be7e5275e061300f49d8d80
prusovak2 <prusova.k2@seznam.cz>
2020-08-11 18:12:02 +0200

    rather useless tests

--- FakerTests/RandomGeneratorAlgsTests.cs
+++ FakerTests/RandomGeneratorAlgsTests.cs
@@ -4,51 +4,60 @@ using System;
 
 namespace FakerTests
 {
+    
     [TestClass]
-    public class RandomGeneratorAlgsTests
+    public class RandomGeneratorAlgTests
     {
-        [TestClass]
-        public class RandomGeneratorAlgTests
+        [TestMethod]
+        public void SplitMixTest()
         {
-            [TestMethod]
-            public void SplitMixTest()
+            Splitmix64 s = new Splitmix64();
+            System.Console.WriteLine(s.Seed);
+            System.Console.WriteLine();
+            for (int i = 0; i < 50; i++)
             {
-                Splitmix64 s = new Splitmix64();
-                System.Console.WriteLine(s.Seed);
-                System.Console.WriteLine();
-                for (int i = 0; i < 50; i++)
-                {
-                    System.Console.WriteLine(s.Next());
-                }
+                System.Console.WriteLine(s.Next());
             }
-            [TestMethod]
-            public void Xoshiro256starstarTest()
+        }
+        [TestMethod]
+        public void Xoshiro256starstarTest()
+        {
+            Xoshiro256starstar s = new Xoshiro256starstar(1);
+            System.Console.WriteLine(s.Seed);
+            System.Console.WriteLine();
+            for (int i = 0; i < 50; i++)
             {
-                Xoshiro256starstar s = new Xoshiro256starstar(1);
-                System.Console.WriteLine(s.Seed);
-                System.Console.WriteLine();
-                for (int i = 0; i < 50; i++)
-                {
-                    ulong r = s.Next();
-                    System.Console.WriteLine(Convert.ToString((long)r, 2));
-                    //ulong mask = 0b_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1000_0000_0000;
-                    //r &= mask;
-                    r >>= 11;
-                    System.Console.WriteLine("{0}", Convert.ToString((long)r,2));
-                    double d = r / (double) 9007199254740992;
-                    Console.WriteLine(r);
-                    Console.WriteLine(d);
-                }
+                ulong r = s.Next();
+                System.Console.WriteLine(Convert.ToString((long)r, 2));
+                //ulong mask = 0b_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1000_0000_0000;
+                //r &= mask;
+                r >>= 11;
+                System.Console.WriteLine("{0}", Convert.ToString((long)r,2));
+                double d = r / (double) 9007199254740992;
+                Console.WriteLine(r);
+                Console.WriteLine(d);
             }
-            [TestMethod]
-            public void ZeroOneDoubleTest()
+        }
+        [TestMethod]
+        public void ZeroOneDoubleTest()
+        {
+            RandomGenerator r = new RandomGenerator();
+            RandomGenerator r2 = new RandomGenerator();
+            for (int i = 0; i < 30; i++)
             {
-                RandomGenerator r = new RandomGenerator();
-                for (int i = 0; i < 30; i++)
-                {
-                    Console.WriteLine(r.ZeroToOneDouble());
-                }
+                Console.WriteLine("r:   {0}", r.ZeroToOneDouble());
+                Console.WriteLine("r2: {0}", r2.ZeroToOneDouble());
+            }
+        }
+        [TestMethod]
+        public void DoubleRangeIdeaTest()
+        {
+            RandomGenerator r = new RandomGenerator();
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(r.DoubleRangePossitiveForATimeBeing(10,1100));
             }
         }
     }
 }
+
---
+++ FakerTests/RandomGeneratorDoubleTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Faker;
+
+
+namespace FakerTests
+{
+    [TestClass]
+    public class RandomGeneratorDoubleTests
+    {
+        [TestMethod]
+        public void DoubleEpsiloEqualTest()
+        {
+            // Initialize two doubles with apparently identical values
+            double double1 = 0.3333333;
+            double double2 = 1d / 3d;
+            bool b = double1.EpsilonEquals(double2);
+            Assert.IsTrue(b);
+
+            double1 = .33333;
+            double2 = 1d / 4d;
+            b = double1.EpsilonEquals(double2);
+            Assert.IsFalse(b);
+
+            double1 = .33333;
+            double2 = 99999999999999990000000.1;
+            b = double1.EpsilonEquals(double2);
+            Assert.IsFalse(b);
+        }
+    }
+}
