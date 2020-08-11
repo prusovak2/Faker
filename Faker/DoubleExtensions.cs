584538e1550e9a3b8699aba543933dd237d41040
prusovak2 <prusova.k2@seznam.cz>
2020-08-11 18:09:06 +0200

    EpsilonEquals to compare doubles added

---
+++ Faker/DoubleExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faker
+{
+    public static class DoubleExtensions
+    {
+        /// <summary>
+        /// compare two double values for equality taking into account relative margin of difference between two values (0.00001 * lowerNumber)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool EpsilonEquals(this double a, double b)
+        {
+            double epsilon;
+            // Define the tolerance for variation in their values based on lower number
+            if (a <= b)
+            {
+                epsilon = Math.Abs(a * 0.00001);
+            }
+            else
+            {
+                epsilon = Math.Abs(b * 0.00001);
+            }
+            Console.WriteLine(epsilon);
+            // Compare the values
+            if (Math.Abs(a - b) <= epsilon)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                
+        }
+    }
+}
