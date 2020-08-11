6ae98ba4b526fc1ebe8c9eb6b57671edaad47274
prusovak2 <prusova.k2@seznam.cz>
2020-08-11 18:11:40 +0200

    able to produce random double values from possitive double range

--- Faker/RandomGenerator.cs
+++ Faker/RandomGenerator.cs
@@ -7,12 +7,39 @@ namespace Faker
     public class RandomGenerator
     {
         private IRandomGeneratorAlg RandomGeneratorAlg { get; set; } = new Xoshiro256starstar();
+        public RandomGenerator()
+        {
+            this.RandomGeneratorAlg = new Xoshiro256starstar();
+        }
+        public RandomGenerator(ulong seed)
+        {
+            this.RandomGeneratorAlg = new Xoshiro256starstar(seed);
+        }
         public double ZeroToOneDouble()
         {
             ulong random = this.RandomGeneratorAlg.Next();
             random >>= 11; //throw away lower 11 bits of uncertain quality, use remaining 53 bits as significand of [0,1) double
-            double result = random / (double) 9007199254740992; // divide by 2^53 - normilize number to [0-1) interval
+            double result = random / (double) 9007199254740992; // divide by 2^53 - normalize number to [0-1) interval
             return result;
+        }
+        public double DoubleRangePossitiveForATimeBeing(double lower, double upper)
+        {
+            //TODO: finish! SOLVE ALL CORNER CASES, THIS IS JUST A  BAREBONE IDEA! NEGATIVE NUMBERS NOT TAKEN INTO ACCOUNT AT ALL!
+            if (lower.EpsilonEquals(upper))
+            {
+                return lower;
+            }
+            if (lower > upper)
+            {
+                double tmp = lower;
+                lower = upper;
+                upper = tmp;
+            }
+            double rangeLenght = upper - lower;
+            double random01 = this.ZeroToOneDouble();
+            double scaled = random01 * rangeLenght;
+            double random = lower + scaled;
+            return random;
         }
     }
 }
