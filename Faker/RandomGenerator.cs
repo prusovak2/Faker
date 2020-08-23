bc3c1c5df87d3a1dc8488ab0579e8f079df09294
prusovak2 <prusova.k2@seznam.cz>
2020-08-23 14:38:49 +0200

    main file of RandomGenerator added

---
+++ Faker/RandomGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faker
+{
+    public partial class RandomGenerator
+    {
+        private IRandomGeneratorAlg RandomGeneratorAlg { get; set; }
+        public RandomGenerator()
+        {
+            this.RandomGeneratorAlg = new Xoshiro256starstar();
+        }
+        public RandomGenerator(ulong seed)
+        {
+            this.RandomGeneratorAlg = new Xoshiro256starstar(seed);
+        }
+        public ulong Seed => this.RandomGeneratorAlg.Seed;
+
+        /// <summary>
+        /// generates a random boolean
+        /// </summary>
+        /// <returns></returns>
+        public bool RandomBool()
+        {
+            double random = this.RandomZeroToOneDouble();
+            if (random < 0.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
