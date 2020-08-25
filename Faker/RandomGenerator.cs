b4083f8995bec61c07cbb8fcf1c66a2cfc4c2129
prusovak2 <prusova.k2@seznam.cz>
2020-08-25 22:17:59 +0200

    GetDefaultRandomFuncForType

--- Faker/RandomGenerator.cs
+++ Faker/RandomGenerator.cs
@@ -17,6 +17,48 @@ namespace Faker
         }
         public ulong Seed => this.RandomGeneratorAlg.Seed;
 
+        public Func<object> GetDefaultRandomFuncForType(object o)
+        {
+            switch (o)
+            {
+                case byte b:
+                    return () => this.RandomByte();
+                case sbyte sb:
+                    return () => this.RandomSbyte();
+                case short s:
+                    return () => this.RandomShort();
+                case ushort us:
+                    return () => this.RandomUshort();
+                case int i:
+                    return () => this.RandomInt();
+                case uint ui:
+                    return () => this.RandomUint();
+                case long l:
+                    return () => this.RandomLong();
+                case ulong ul:
+                    return () => this.RandomUlong();
+                case float f:
+                    return () => this.RandomFloat();
+                case double d:
+                    return () => this.RandomDouble();
+                case decimal d:
+                    return () => this.RandomDecimal();
+                case char c:
+                    return () => this.RandomChar();
+                case bool b:
+                    return () => this.RandomBool();
+                case string s:
+                    return () => this.RandomString();
+                case DateTime dt:
+                    return () => this.RandomDateTime();
+                case Guid g:
+                    return () => this.RandomGuid();
+                default:
+                    return null;
+                 
+            }
+        }
+
         /// <summary>
         /// generates a random boolean
         /// </summary>
@@ -29,6 +71,22 @@ namespace Faker
                 return true;
             }
             return false;
+        }
+        public string RandomString()
+        {
+            throw new NotImplementedException();
+        }
+        public Guid RandomGuid()
+        {
+            throw new NotImplementedException();
+        }
+        public DateTime RandomDateTime()
+        {
+            throw new NotImplementedException();
+        }
+        public char RandomChar()
+        {
+            throw new NotImplementedException();
         }
     }
 }
