74eb650d325ec1ec38ac85b1b8be40f4a63b2ce8
prusovak2 <prusova.k2@seznam.cz>
2020-08-25 12:06:40 +0200

    FakerException added

---
+++ Faker/FakerException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faker
+{
+    public class FakerException : Exception
+    {
+        public FakerException() : base() { }
+        public FakerException(string message): base(message) { }
+        public FakerException(string message, Exception innerException): base(message, innerException) { }
+    }
+}
