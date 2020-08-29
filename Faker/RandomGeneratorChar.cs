4427665b686023f9dda224e54219d4d98e972a66
prusovak2 <prusova.k2@seznam.cz>
2020-08-29 21:34:14 +0200

    commented

--- Faker/RandomGeneratorChar.cs
+++ Faker/RandomGeneratorChar.cs
@@ -8,60 +8,99 @@ namespace Faker
 {
     public partial class RandomGenerator
     {
+        /// <summary>
+        ///generates a random char from interval[lower, upper] <br/>
+        /// when lower/upper bound is not specified, char.MinValue/char.MaxValue is used 
+        /// <param name="lower"></param>
+        /// <param name="upper"></param>
+        /// <returns></returns>
         public char RandomChar(char lower = char.MinValue, char upper = char.MaxValue)
         {
             ushort randomUshort = this.RandomUshort(lower, upper);
             return (char)randomUshort;
         }
+        /// <summary>
+        /// generates a random char representing a digit
+        /// </summary>
+        /// <returns></returns>
         public char RandomDigit()
         {
-            int digit = this.RandomInt(48, 57); //ascii for digits
+            int digit = this.RandomInt(48, 57); //ASCII for digits
             return (char)digit;
         }
+        /// <summary>
+        /// generates a random char representing a lowercase letter
+        /// </summary>
+        /// <returns></returns>
         public char RandomLowerCaseLetter()
         {
-            int ord = this.RandomInt(97, 122); //ascii for lower case
+            int ord = this.RandomInt(97, 122); //ASCII for lowercase
             return (char)ord;
         }
+        /// <summary>
+        /// generates a random char representing a uppercase letter
+        /// </summary>
+        /// <returns></returns>
         public char RandomUpperCaseLetter()
         {
-            int ord = this.RandomInt(65, 90); //ascii for upper case
+            int ord = this.RandomInt(65, 90); //ASCII for uppercase
             return (char)ord;
         }
+        /// <summary>
+        /// generates a random char representing a Latin letter
+        /// </summary>
+        /// <returns></returns>
         public char RandomLetter()
         {
             int ord = this.RandomInt(65, 116);
-            if (ord > 90) //shift lowercase to ascii range for lowercase
+            if (ord > 90) //shift lowercase to ASCII range for lowercase
             {
                 ord += 6;
             }
             return (char)ord;
         }
+        /// <summary>
+        /// generates a random letter or digit
+        /// </summary>
+        /// <returns></returns>
         public char RandomAlphanumericChar()
         {
             int ord = this.RandomInt(55, 116); //interval large enough to contain all alphanum., shifted to beginning of upper case letters
-            if (ord > 90) //shift lowercase to ascii range for lowercase
+            if (ord > 90) //shift lowercase to ASCII range for lowercase
             {
                 ord += 6;
             }
-            else if (ord < 65) //shift digit to ascii range for digit
+            else if (ord < 65) //shift digit to ASCII range for digit
             {
                 ord -= 7;
             }
             return (char)ord;
         }
+        /// <summary>
+        /// generates a random char from ASCII range
+        /// </summary>
+        /// <returns></returns>
         public char RandomAsciiChar()
         {
             int ord = this.RandomInt(0, 127);
             return (char)ord;
         }
-            
+        /// <summary>
+        /// generates a random char representing a vowel <br/>
+        /// including y
+        /// </summary>
+        /// <returns></returns>
         public char RandomVowel()
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u','y' };
             int index = this.RandomInt(0, vowels.Length-1);
             return vowels[index];
         }
+        /// <summary>
+        /// generates a random char representing a consonant <br/>
+        /// excluding y
+        /// </summary>
+        /// <returns></returns>
         public char RandomConsonant()
         {
             //TODO: is Y vowel or consonant?
@@ -72,7 +111,9 @@ namespace Faker
         //TODO: white/ non white random char?
         public string RandomString()
         {
-            throw new NotImplementedException();
+            char[] c = this.RandomList<char>(255).ToArray();
+            return new string(c);
         }
+        
     }
 }
--- Faker/RandomGeneratorCollection.cs
+++ Faker/RandomGeneratorCollection.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Collections.ObjectModel;
+using System.Collections;
 
 namespace Faker
 {
     public partial class RandomGenerator
     {
+        /// <summary>
+        /// returns count, when precise is true or random int less or equal to count when precise is false 
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="precise">should be used precisely the count?</param>
+        /// <returns></returns>
         internal int CountToUse(int count, bool precise)
         {
             int countToUse;
@@ -20,6 +27,16 @@ namespace Faker
             }
             return countToUse;
         }
+        /// <summary>
+        /// returns collection of TMembers produced by Func<TMember> randomFunc <br/>
+        /// when precise is true, count is used as a size of returned collection <br/>
+        /// otherwise a random number less or equal to count is generated and used as a count
+        /// </summary>
+        /// <typeparam name="TMember"></typeparam>
+        /// <param name="randomFunc"></param>
+        /// <param name="count">count of members in returned collection</param>
+        /// <param name="precise"></param>
+        /// <returns></returns>
         public ICollection<TMember> RandomCollection<TMember>(Func<TMember> randomFunc, int count, bool precise = true)
         {
             ICollection<TMember> result = new Collection<TMember>();
@@ -32,6 +49,18 @@ namespace Faker
             }
             return result;
         }
+        /// <summary>
+        /// returns collection of TMembers produced by Func<TMember, TMember, TMembe> randomFunc <br/>
+        /// when precise is true, count is used as a size of returned collection <br/>
+        /// otherwise a random number less or equal to count is generated and used as a count
+        /// </summary>
+        /// <typeparam name="TMember"></typeparam>
+        /// <param name="randomFunc"></param>
+        /// <param name="lower">lower bound passed to random function</param>
+        /// <param name="upper">upper bound passed to random function</param>
+        /// <param name="count">count of members in returned collection</param>
+        /// <param name="precise"></param>
+        /// <returns></returns>
         public ICollection<TMember> RandomCollection<TMember>(Func<TMember, TMember, TMember> randomFunc, TMember lower, TMember upper, int count, bool precise = true)
         {
             ICollection<TMember> result = new Collection<TMember>();
@@ -43,6 +72,16 @@ namespace Faker
             }
             return result;
         }
+        /// <summary>
+        /// returns collection of random members produced by a default random function for TMember type <br/>
+        /// when precise is true, count is used as a size of returned collection <br/>
+        /// otherwise a random number less or equal to count is generated and used as a count
+        /// </summary>
+        /// <exception cref="FakerException">Throws a FakerException, when there is no default random function for TMember type</exception>
+        /// <typeparam name="TMember"></typeparam>
+        /// <param name="count"></param>
+        /// <param name="precise"></param>
+        /// <returns></returns>
         public ICollection<TMember> RandomCollection<TMember>(int count, bool precise = true)
         {
             Type type = typeof(TMember);
@@ -60,7 +99,17 @@ namespace Faker
             }
             return result;
         }
-        public IList<TMember> RandomList<TMember>(int count)
+        /// <summary>
+        ///  returns a list of random members produced by a default random function for TMember type <br/>
+        /// when precise is true, count is used as a count of returned list <br/>
+        /// otherwise a random number less or equal to count is generated and used as a count
+        /// </summary>
+        /// <exception cref="FakerException">Throws a FakerException, when there is no default random function for TMember type</exception>
+        /// <typeparam name="TMember"></typeparam>
+        /// <param name="count"></param>
+        /// <param name="precise"></param>
+        /// <returns></returns>
+        public IList<TMember> RandomList<TMember>(int count, bool precise = false)
         {
             Type type = typeof(TMember);
             Func<object> randomFunc = this.GetDefaultRandomFuncForType(type);
@@ -69,12 +118,83 @@ namespace Faker
                 throw new FakerException($"{typeof(TMember)} type does not have a default random function.");
             }
             IList<TMember> result = new List<TMember>();
-            for (int i = 0; i < count; i++)
+            int countToUse = this.CountToUse(count, precise);
+            for (int i = 0; i < countToUse; i++)
             {
                 TMember next = (TMember)randomFunc();
                 result.Add(next);
             }
             return result;
         }
+        /// <summary>
+        /// returns a list of TMembers produced by Func<TMember> randomFunc <br/>
+        /// when precise is true, count is used as a count of returned list <br/>
+        /// otherwise a random number less or equal to count is generated and used as a count
+        /// </summary>
+        /// <typeparam name="TMember"></typeparam>
+        /// <param name="randomFunc"></param>
+        /// <param name="count"></param>
+        /// <param name="precise"></param>
+        /// <returns></returns>
+        public IList<TMember> RandomList<TMember>(Func<TMember> randomFunc, int count, bool precise = true)
+        {
+            IList<TMember> result = new List<TMember>();
+
+            int countToUse = this.CountToUse(count, precise);
+            for (int i = 0; i < countToUse; i++)
+            {
+                TMember next = randomFunc();
+                result.Add(next);
+            }
+            return result;
+        }/// <summary>
+         /// returns a list of TMembers produced by Func<TMember, TMember, TMembe> randomFunc <br/>
+         /// when precise is true, count is used as a count of returned list <br/>
+         /// otherwise a random number less or equal to count is generated and used as a count
+         /// </summary>
+         /// <typeparam name="TMember"></typeparam>
+         /// <param name="randomFunc"></param>
+         /// <param name="lower">lower bound passed to random function</param>
+         /// <param name="upper">upper bound passed to random function</param>
+         /// <param name="count"></param>
+         /// <param name="precise"></param>
+         /// <returns></returns>
+        public IList<TMember> RandomList<TMember>(Func<TMember, TMember, TMember> randomFunc, TMember lower, TMember upper, int count, bool precise = true)
+        {
+            IList<TMember> result = new List<TMember>();
+            int countToUse = this.CountToUse(count, precise);
+            for (int i = 0; i < countToUse; i++)
+            {
+                TMember next = randomFunc(lower, upper);
+                result.Add(next);
+            }
+            return result;
+        }
+        //TODO: Random enumerable?
+      /*  public IEnumerable<TMember> RandomEnumerable<TMember>(Func<TMember> randomFunc)
+        {
+            RandomEnumerableClass < TMember > Enumerable = new RandomEnumerableClass<TMember>(randomFunc);
+            return Enumerable;
+        }
+        private class RandomEnumerableClass<TMemeber> : IEnumerable<TMemeber>
+        {
+            internal Func<TMemeber> randomFunc;
+            public RandomEnumerableClass(Func<TMemeber> func)
+            {
+                this.randomFunc = func;
+            }
+            public IEnumerator<TMemeber> GetEnumerator()
+            {
+                while (true)
+                {
+                    yield return this.randomFunc();
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }*/
     }
 }
