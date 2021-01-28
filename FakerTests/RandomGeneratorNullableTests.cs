47dfbb5f55397f76c2bc6b819d0c726da28ab511
prusovak2 <prusova.k2@seznam.cz>
2021-01-28 19:11:17 +0100

    AlwaysNullNeverNull, InvalidProbability, NullableByteTest tests

--- FakerTests/RandomGeneratorNullableTests.cs
+++ FakerTests/RandomGeneratorNullableTests.cs
@@ -9,9 +9,76 @@ namespace FakerTests
     [TestClass]
     public class RandomGeneratorNullableTests
     {
-        public partial class RandomBasicTypes
-        {
 
+        [TestMethod]
+        public void AlwaysNullNeverNull()
+        {
+            RandomGenerator r = new RandomGenerator();
+            //always null
+            for (int i = 0; i < 10000; i++)
+            {
+                int? ni = r.Random.NullableGeneric<int>(1f);
+                Assert.IsNull(ni);
+            }
+            //never null
+            for (int i = 0; i < 10000; i++)
+            {
+                int? ni = r.Random.NullableGeneric<int>(0f);
+                Assert.IsNotNull(ni);
+            }
         }
+
+        [TestMethod]
+        public void InvalidProbability()
+        {
+            RandomGenerator r = new RandomGenerator();
+            Assert.ThrowsException<ArgumentException>(() => { bool? nb = r.Random.NullableGeneric<bool>(42); });
+            Assert.ThrowsException<ArgumentException>(() => { bool? nb = r.Random.NullableGeneric<bool>(-73); });
+        }
+
+        [TestMethod]
+        public void NullableByteTest()
+        {
+            RandomGenerator r = new RandomGenerator();
+            byte? x;
+            byte lower = 42;
+            byte upper = 73;
+            int nullCounter = 0;
+            for (int i = 0; i < 500; i++)
+            {
+                x = r.Random.NullableByte(5/10f, lower, upper);
+                if (x is object)
+                {
+                    Console.WriteLine(x);
+                }
+                Assert.IsTrue((x >= lower && x <= upper) || (x is null));
+                if(x is null)
+                {
+                    nullCounter++;
+                    Console.WriteLine("null");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Assert.IsFalse(nullCounter == 0);
+            nullCounter = 0;
+            for (int i = 0; i < 500; i++)
+            {
+                x = r.Random.NullableByte(5 / 10f);
+                if (x is object)
+                {
+                    Console.WriteLine(x);
+                }
+                if (x is null)
+                {
+                    nullCounter++;
+                    Console.WriteLine("null");
+                }
+            }
+            Assert.IsFalse(nullCounter == 0);
+        }
+
+
+        
     }
 }
