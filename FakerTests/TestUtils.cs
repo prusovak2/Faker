170dc33854c04ca77a0d19c77fe6c94bbd5abb66
prusovak2 <prusova.k2@seznam.cz>
2021-01-31 17:47:22 +0100

    TestUtils InstanceToString method

---
+++ FakerTests/TestUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+namespace FakerTests
+{
+    public static class TestUtils
+    {
+        public static string InstanceToString<T>(T instance)
+        {
+            Type type = typeof(T);
+            string res = type.Name + ":\n\t";
+            List<MemberInfo> memberInfos = type.GetMembers().Where(memberInfo => (memberInfo is PropertyInfo || memberInfo is FieldInfo)).ToList();
+            foreach (var memInfo in memberInfos)
+            {
+                res += (memInfo.Name + ": ");
+                if (memInfo is PropertyInfo propInfo)
+                {
+                    res += propInfo.GetValue(instance).ToString();
+                }
+                else if (memInfo is FieldInfo fieldInfo)
+                {
+                    res += fieldInfo.GetValue(instance).ToString();
+                }
+                else
+                {
+                    throw new NotImplementedException("Unexpected");
+                }
+                res += "\n\t";
+            }
+            return res;
+        }
+    }
+}
