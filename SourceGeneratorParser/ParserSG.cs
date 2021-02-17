c803d3948606e1928488da844139452e2f7d20f7
prusovak2 <prusova.k2@seznam.cz>
2021-02-17 21:59:48 +0100

    Playing with source generator

--- SourceGeneratorParser/ParserSG.cs
+++ SourceGeneratorParser/ParserSG.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
 using System;
 using System.IO;
 using System.Text;
@@ -9,12 +10,16 @@ namespace SourceGeneratorParser
     [Generator]
     public class ParserSG : ISourceGenerator
     {
+        internal const string inMethodIndent = "            ";
+        internal const string inClassIndent = "        ";
+        internal Dictionary<string, HashSet<string>> dirsWithFiles = new();
+
         public void Initialize(GeneratorInitializationContext context) { }
 
         public void Execute(GeneratorExecutionContext context)
         {
             StringBuilder sb = new StringBuilder();
-            string indent = "            ";
+           
             sb.Append(@"
 using System;
 namespace Faker {
@@ -22,28 +27,111 @@ namespace Faker {
     {
         public static partial string GetFileText()
         {
- ");
-            sb.AppendLine($"{indent}Console.WriteLine(\"ABRAKA\");");
+");
+            sb.AppendLine($"{inMethodIndent}Console.WriteLine(\"ABRAKA\");");
+            sb.AppendLine($"{inMethodIndent}Console.WriteLine(\"DABRA\");");
             foreach (AdditionalText file in context.AdditionalFiles)
             {
                 //Path.GetExtension(file.Path);
-                sb.AppendLine($@"{indent}Console.WriteLine(""{file.Path.Replace('\\', '/')}"");");
+                string dirName = new DirectoryInfo(Path.GetDirectoryName(file.Path)).Name;
+                string fileName = Path.GetFileNameWithoutExtension(file.Path);
+                sb.AppendLine($@"{inMethodIndent}Console.WriteLine(""{dirName}"");");
+                sb.AppendLine($@"{inMethodIndent}Console.WriteLine(""{fileName}"");");
+
+                sb.AppendLine($@"{inMethodIndent}Console.WriteLine(""{file.Path.Replace('\\', '/')}"");");
                 var text = file.GetText(context.CancellationToken).ToString();
                 //char[] delims = { ' ', '\t', '\n' };
                 var words = text.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
                 foreach (var item in words)
                 {
-                    sb.AppendLine($@"{indent}Console.WriteLine(""{item}"");");
+                    sb.AppendLine($@"{inMethodIndent}Console.WriteLine(""{item}"");");
                 }
+                
                
             }
-            sb.AppendLine($"{indent}return \"ABRAKA\";");
+            sb.AppendLine($"{inMethodIndent}return \"ABRAKA\";");
             sb.Append(@"
         }
     }
 }");
-            
             context.AddSource("Generated.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
+
+            string initMethodSource;
+            string partialClassesSource;
+            (initMethodSource, partialClassesSource) = this.CreateSourceForRGPartialClasses();
+            context.AddSource("GeneratedInitMethod.cs", SourceText.From(initMethodSource, Encoding.UTF8));
+            context.AddSource("GeneratedPartialClasses.cs", SourceText.From(partialClassesSource, Encoding.UTF8));
+
         }
+
+        internal bool AddFileRecord(AdditionalText file)
+        {
+            string dirName = new DirectoryInfo(Path.GetDirectoryName(file.Path)).Name;
+            string fileName = Path.GetFileNameWithoutExtension(file.Path);
+            if (!this.dirsWithFiles.ContainsKey(dirName))
+            {
+                this.dirsWithFiles.Add(dirName, new HashSet<string>());
+            }
+            if (this.dirsWithFiles[dirName].Contains(fileName))
+            {
+                return false;
+            }
+            this.dirsWithFiles[dirName].Add(fileName);
+            return true;
+        }
+
+        internal void ParseFileToSourceCode(AdditionalText file, StringBuilder sb, GeneratorExecutionContext context)
+        {
+            var text = file.GetText(context.CancellationToken).ToString();
+            var words = text.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            //TODO:parse file to dictionary
+        }
+
+        internal (string, string) CreateSourceForRGPartialClasses()
+        {
+            StringBuilder initBuilder = new();
+            initBuilder.Append(@"
+using System;
+namespace Faker
+{
+    public partial class RandomGenerator
+    {
+         internal partial void RandomGeneratorInitializationGenerated()
+        {
+");
+            StringBuilder partialClassesBuilder = new();
+            partialClassesBuilder.Append(@"
+using System;
+namespace Faker
+{
+    public partial class RandomGenerator
+    {");
+
+            foreach (var pair in this.dirsWithFiles)
+            {
+                string dirName = pair.Key;
+                initBuilder.AppendLine($@"{inMethodIndent}this.{dirName} = new Random{dirName}(this);");
+                partialClassesBuilder.AppendLine($"{inClassIndent}public Random{dirName} {dirName} {{ get; private set; }}");
+                partialClassesBuilder.AppendLine($"{inClassIndent}public class Random{dirName}");
+                partialClassesBuilder.AppendLine($"{inClassIndent}{{");  //curly bracket opening partial class definition
+
+                foreach (var fileName in pair.Value)
+                {
+                    //TODO:generate method per file in the dir
+                }
+
+                partialClassesBuilder.AppendLine($"{inClassIndent}}}");  //curly bracket closing partial class definition
+            }
+
+            initBuilder.Append(@"    
+        }
+    }
+}");
+            partialClassesBuilder.Append(@"
+    }
+}");
+            return (initBuilder.ToString(), partialClassesBuilder.ToString()) ;
+        }
+
     }
 }
