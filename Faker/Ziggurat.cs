0d456dff1e20c8d9127b2079eea643e71e2d7315
prusovak2 <prusova.k2@seznam.cz>
2021-02-21 10:48:33 +0100

    magical constants added, Generate implementation started

--- Faker/Ziggurat.cs
+++ Faker/Ziggurat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,9 @@ namespace Faker
     {
         protected const int numBlocks = 128;
 
-        protected const double r = 3.442619855899;
+        protected abstract double r { get; }
 
-        protected const double area = 9.91256303526217e-3;
+        protected abstract double area { get; }
 
         protected static double[] x { get; set; } = new double[numBlocks + 1];
 
@@ -37,14 +38,19 @@ namespace Faker
             ulong uMantis = generatorAlg.Next();
             uMantis >>= 11; //throw away lower 11 bits of uncertain quality
 
-           
             ulong uSector = generatorAlg.Next();
-            ulong uppermostBit = uSector & 0x8000_0000_0000_0000; //one and 63 zeros
-            ulong bitsOfDoubleOne = 0x3ff0_0000_0000_0000UL;
-            double sign = BitConverter.Int64BitsToDouble(unchecked((long)(bitsOfDoubleOne | uppermostBit)));
 
+            //use the most significant bit (bit 63) to determine sign
+            //necessary for normal distribution as alg only covers its right half, to get values from left half, sign is randomly generated
+            //empty operation returning 1 for exponential distribution 
+            double sign = Sign(uSector);
 
-            Sign(42);
+            //get bits 62 to 56 (7 bits determining sector number from {0, 1, ..., 127} )
+            uSector >>= 56;
+            uSector &= 0x7F; //0111_1111
+            Debug.Assert((uSector >= 0) && (uSector <= 127));
+
+            
             throw new NotImplementedException();
         }
 
@@ -59,6 +65,9 @@ namespace Faker
 
     sealed class NormalDistribution : Ziggurat
     {
+        protected override double r => 3.442619855899;
+        protected override double area => 9.91256303526217e-3;
+
         protected override double ProbabilityDensityFunction(double x)
         {
             throw new NotImplementedException();
@@ -76,7 +85,38 @@ namespace Faker
 
         protected override double Sign(ulong random)
         {
+            ulong uppermostBit = random & 0x8000_0000_0000_0000; //one and 63 zeros
+            ulong bitsOfDoubleOne = 0x3ff0_0000_0000_0000UL; //double one in binary representation
+            double sign = BitConverter.Int64BitsToDouble(unchecked((long)(bitsOfDoubleOne | uppermostBit)));
+            Debug.Assert((sign == 1) || (sign == -1));
+            return sign;
+        }
+    }
+
+    sealed class ExponentialDistribution : Ziggurat
+    {
+        protected override double r => 7.69711747013104972;
+
+        protected override double area => 0.0039496598225815571993;
+
+        protected override double ProbabilityDensityFunction(double x)
+        {
             throw new NotImplementedException();
+        }
+
+        protected override double ProbabilytyDensityInverz(double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double SampleTail()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double Sign(ulong random = 0)
+        {
+            return 1;
         }
     }
 }
